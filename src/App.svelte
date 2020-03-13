939080397607ecbaaed7a33707c8435207561c7f
gentilmente <egentilemontes@gmail.com>
2020-03-12 21:05:38 -0300

    componente dentro de componente App ok

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Start from '/Start.svelte';
+	import Start from './Start.svelte';
 </script>
 
 <main>
@@ -8,19 +8,13 @@
 </main>
 <Start/>
 
+
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
