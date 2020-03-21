cb079cd6d5d8de38911b2d9459b60822485cd408
gentilmente <egentilemontes@gmail.com>
2020-03-20 23:27:11 -0300

    css order

--- src/App.svelte
+++ src/App.svelte
@@ -8,27 +8,21 @@
 <main>
 	<h1><a href='/'>Repartija</a></h1>
 	<p class="subtitle">No mas ebrios haciendo cuentas</p>
+
+	{#if menu === 1}
+	<Start on:click = {()=> menu = 2} />
+		{:else if menu === 2}
+		<Form/>
+		<!--{console.log(menu)}-->
+		{:else}
+		<h1>
+			Page Not Found
+		</h1>
+		{/if}
+		
 </main>
 
-{#if menu === 1}
-<Start on:click = {()=> menu = 2} />
-{:else if menu === 2}
-<Form/>
-<!--{console.log(menu)}-->
-{:else}
-<h1>
-	Page Not Found
-</h1>
-{/if}
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
--- src/Start.svelte
+++ src/Start.svelte
@@ -4,7 +4,7 @@
     colocas tus datos y deslizás "soy el último",
     luego al agregarte verás la magia 👻 </p>
 
-  <h1><button class="start" href='/' on:click>comenzar</button> </h1>
+  <button on:click><span>comenzar </span></button>
 
 </main>
 
@@ -16,13 +16,43 @@
     text-shadow: 1px 1px 7px black;
   }
 
-  .start {
-    color: azure;
+  button {
+    display: inline-block;
+    border-radius: 4px;
+    background-color: #f4511e;
+    border: none;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 28px;
+    padding: 20px;
+    width: 200px;
+    transition: all 0.5s;
+    cursor: pointer;
+    margin: 5px;
+  }
+
+  button span {
+    cursor: pointer;
+    display: inline-block;
     position: relative;
-    width: 90%;
-    background-color: black;
-    -moz-transition-duration: 0.5s;
-    -webkit-transition-duration: 0.5s;
-    margin-bottom: 20px;
+    transition: 0.5s;
+  }
+
+  button span:after {
+    content: '\00bb';
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: -20px;
+    transition: 0.5s;
+  }
+
+  button:hover span {
+    padding-right: 25px;
+  }
+
+  button:hover span:after {
+    opacity: 1;
+    right: 0;
   }
 </style>
